392937fbeaa4a88f00d2c925d72f965eb73e7e31
Oliver Feher <olcsee@gmail.com>
2020-01-29 18:56:23 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  attr_accessor :name, :artist, :genre
+  
+  def initialize (name, artist, genre)
+    @name = name
+    @artists = artist
+    @genre = genre
+    
+    @@count += 1
+    @@genres << genre
+    @@artist << artist
+  end
+  
+  def count
+    @@count
+  end
+  
+  def genres
+    @@genres.uniq
+  end
+  
+  def artists
+    @@artists.uniq
+  end
+  
+  def genre_count
+    @@genres.map { |genre, count| genre count +=1}.to_h
+  end
+end
